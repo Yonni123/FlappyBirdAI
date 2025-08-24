71c48e0a245c50bdec12e59c76fae0e8bca2e794
Yonan <yonan.adnan707@gmail.com>
2025-08-24 13:26:04 +0200

    Add bird detection

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ import cv2
 import time
 import numpy as np
 
+# GLOBALS
+
 HSV_dict = {
     "outlines": (np.array([166, 64, 74]), np.array([179, 110, 105])),
     "pipes": (np.array([33, 90, 106]), np.array([48, 200, 255])),
+    "bird": [
+        (np.array([2, 139, 88]), np.array([13, 255, 255])),    # Red
+        (np.array([96, 108, 148]), np.array([108, 200, 255])),  # Blue
+        (np.array([14, 116, 123]), np.array([20, 226, 255]))   # Yellow
+    ]
 }
 
 floor_y = None
+bird_index = None
 
 class pipe:
     def __init__(self, x, y, w, h, syt, syb):   # Safe y-top, Safe y-bottom
@@ -40,6 +48,29 @@ def segment_frame(frame, target):
     return mask
 
 
+def draw_screen_info(frame, floor_y, pipes, bird=None):
+    for p in pipes:
+        x, y, w, h = p.x, p.y, p.w, p.h
+        # Draw bounding boxes
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 4)
+        cv2.putText(frame, f"({x},{y})", (x, p.syb - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+        # Draw safe zones
+        cv2.line(frame, (x, p.syt), (x + w, p.syt), (0, 0, 255), 4)
+        cv2.line(frame, (x, p.syb), (x + w, p.syb), (0, 0, 255), 4)
+
+    # Draw the floor square
+    if floor_y is not None:
+        cv2.rectangle(frame, (0, floor_y), (frame.shape[1], frame.shape[0]), (0, 0, 255), 2)
+
+    # Draw the bird
+    if bird is not None:
+        x, y, w, h = bird
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 4)
+        cv2.putText(frame, f"({x},{y})", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+    return frame
+
+
 def detect_floor_y_position(screen, screen_w):
     mask = segment_frame(screen, "outlines")
 
@@ -55,7 +86,7 @@ def detect_floor_y_position(screen, screen_w):
                 max_w_contour = contour
         if max_w_contour is not None:
             x, y, w, h = cv2.boundingRect(max_w_contour)
-            if screen_w - w < 50:   # Floor width should be close to screen width
+            if screen_w - w < 100:   # Floor width should be close to screen width
                 return y + h - 5
     else:
         return None
@@ -70,7 +101,6 @@ def detect_pipes(screen, floor_y):
 
     # Make everything under the floor black
     mask[floor_y:, :] = 0
-    cv2.imshow("Pipes Mask", mask)
 
     # Find contours of the pipes
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -114,6 +144,35 @@ def process_pipes(screen, floor_y, safety_margin=1):
     return connected_pipes
 
 
+def detect_bird(screen):
+    global HSV_dict, floor_y
+
+    mask = segment_frame(screen, "outlines")
+    kernel = np.ones((7, 7), np.uint8)
+
+    mask = cv2.erode(mask, kernel, iterations=1)
+    mask = cv2.dilate(mask, kernel, iterations=5)
+    mask = cv2.erode(mask, kernel, iterations=3)
+
+    # Crop the sides because pipe outlines might ruin if they are partially visble
+    # The bird will never be there anyway
+    h, w = mask.shape[:2]
+    cut = int(0.1 * w)
+    mask[:, :cut] = 0
+    mask[:, w-cut:] = 0
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if not contours:
+        return None
+
+    # Assume the bird is the largest contour
+    largest_contour = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(largest_contour)
+
+    cv2.imshow("BirdMask", mask)
+    return (x, y, w, h)
+
+
 
 def action_function(self, screen, game_FPS, counter, time_ms):
     global HSV_dict, floor_y
@@ -127,21 +186,10 @@ def action_function(self, screen, game_FPS, counter, time_ms):
             print("Floor y-position not detected yet.")
             return
         
-    # Detect pipes
     pipes = process_pipes(screen, floor_y)
-    for p in pipes:
-        x, y, w, h = p.x, p.y, p.w, p.h
-        # Draw bounding boxes
-        cv2.rectangle(screen, (x, y), (x + w, y + h), (0, 255, 255), 4)
-        cv2.putText(screen, f"({x},{y})", (x, p.syb - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
-        # Draw safe zones
-        cv2.line(screen, (x, p.syt), (x + w, p.syt), (0, 0, 255), 4)
-        cv2.line(screen, (x, p.syb), (x + w, p.syb), (0, 0, 255), 4)
+    bird = detect_bird(screen)
 
-    # Draw the floor square
-    if floor_y is not None:
-        cv2.rectangle(screen, (0, floor_y), (self.width, self.height), (0, 0, 255), 2)
-
+    screen = draw_screen_info(screen, floor_y, pipes, bird)
     cv2.setWindowTitle("GameFrame", f"Game FPS: {game_FPS:.2f} | Frame Counter: {counter:.0f} | Time (ms): {time_ms:.0f}")
     cv2.imshow("GameFrame", screen)
 
